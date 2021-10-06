9ee0d54974eeb9794d4629b9102d69ea4008165e
Lucas Santiago <lu.santi.oli@gmail.com>
2021-10-06 17:02:10 -0300

    feat: main folder added

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+ 
+class Root(Tk):
+    def __init__(self):
+        super(Root,self).__init__()
+ 
+        self.title("Launcher Options Deck")
+        self.minsize(800, 600)
+ 
+root = Root()
+root.mainloop()
