4b96b38fe13b76c5369a5c9a3733d48c623dc4d7
mrnaru300 <bruno.santi.oli@gmail.com>
2021-10-13 19:51:49 -0300

    feat: Option data class

--- src/utils/commands.py
+++ src/utils/commands.py
@@ -1,6 +1,14 @@
+from typing import Any, Optional
 from xml.etree import ElementTree
+from dataclasses import dataclass
 
-class CommandParser():
+@dataclass
+class OptionData():
+    name: str
+    command: str
+    descrition: str
+
+class OptionParser():
 
     def __init__(self, path : str) -> None:
         self.commands = ElementTree.parse(path)
@@ -13,5 +21,11 @@ class CommandParser():
         desc.text = descrition
         element = self.commands.getroot().append(new_option)
 
-    def find_with_name(self, name: str):
-        return self.commands.find(f"option[name={name}]")
+    def find(self, name: str) -> OptionData:
+        el = self.commands.find(f"option[name={name}]")
+        if el == None: return
+
+        command = el.find("command").text
+        description = el.find("description").text
+
+        return OptionData(name=name, command=command, descrition=description)
